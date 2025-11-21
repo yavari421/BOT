456e1d55ea49a9c5b7c35c55e22e6cfa6f479676
مهندس فاطمه یاوری <150955554+yavari421@users.noreply.github.com>
2025-11-21 09:33:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,6 @@ bot = telebot.TeleBot(TOKEN)
 def start(message):
     bot.send_message(message.chat.id, "سلام! لطفا شماره تماس خود را ارسال کنید.")
 
-@bot.message_handler(content_types=['contact'])
-def contact(message):
-    with open("contacts.csv", "a", encoding="utf-8") as f:
-        f.write(f"{message.from_user.id},{message.from_user.first_name},{message.contact.phone_number}\n")
-    bot.send_message(message.chat.id, "شماره شما ثبت شد.\nلینک: https://example.com")
-
-@bot.message_handler(commands=['showcontacts'])
-def show_contacts(message):
-    try:
-        with open("contacts.csv", "r", encoding="utf-8") as f:
-            data = f.read()
-        if data:
-            bot.send_message(message.chat.id, f"شماره‌ها:\n{data}")
-        else:
-            bot.send_message(message.chat.id, "فایل خالی است.")
-    except FileNotFoundError:
-        bot.send_message(message.chat.id, "فایل موجود نیست.")
-
-
-
 conn = sqlite3.connect("contacts.db")
 cursor = conn.cursor()
 cursor.execute("""
@@ -52,5 +32,27 @@ def contact(message):
     cursor.execute("INSERT INTO contacts VALUES (?,?,?)", (telegram_id, name, phone))
     conn.commit()
     bot.send_message(message.chat.id, "شماره شما ثبت شد.\nلینک: https://example.com")
+    
+# @bot.message_handler(content_types=['contact'])
+# def contact(message):
+#     with open("contacts.csv", "a", encoding="utf-8") as f:
+#         f.write(f"{message.from_user.id},{message.from_user.first_name},{message.contact.phone_number}\n")
+#     bot.send_message(message.chat.id, "شماره شما ثبت شد.\nلینک: https://example.com")
+
+@bot.message_handler(commands=['showcontacts'])
+def show_contacts(message):
+    try:
+        with open("contacts.csv", "r", encoding="utf-8") as f:
+            data = f.read()
+        if data:
+            bot.send_message(message.chat.id, f"شماره‌ها:\n{data}")
+        else:
+            bot.send_message(message.chat.id, "فایل خالی است.")
+    except FileNotFoundError:
+        bot.send_message(message.chat.id, "فایل موجود نیست.")
+
+
+
+
 
 bot.polling()
